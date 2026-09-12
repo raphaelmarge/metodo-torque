a4a4d53e8225f0f1f4a6d8cf83e3a54de2e0445c
Toroquefitness <raphaelmarge@gmail.com>
2026-09-12 09:41:29 -0300

    chore(release): precache Central Pro em mt-v830

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@ importScripts("assets/content.js");
  * a resposta que vinha sempre igual.
  *
  * tests/test-versao.js não deixa este número ficar diferente do versao.js. */
-var VERSION = "mt-v829";
+var VERSION = "mt-v830";
 var PRECACHE = "precache-" + VERSION;
 var RUNTIME = "runtime-" + VERSION;
 // O leitor de imagem das Medidas pela câmera tem ~17 MB e vive numa cache
@@ -53,6 +53,8 @@ var CORE = [
   "assets/personal-onboarding.css",
   "assets/personal-torque-one.css",
   "assets/personal-torque-one.js",
+  "assets/personal-pro-suite.css",
+  "assets/personal-pro-suite.js",
   "assets/postural-core.js",
   "assets/postural-store.js",
   "assets/personal-postural.js",
